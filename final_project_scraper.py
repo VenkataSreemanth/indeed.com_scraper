ddfafbe49a37c6fc38d692084cb26c9267d7ac67
SarBH <saritahedaya@gmail.com>
2020-11-05 23:09:02 -0500

    made changes to support whn job has no description

--- final_project_scraper.py
+++ final_project_scraper.py
@@ -16,6 +16,8 @@ def run_scraper(url,page_num, job_type):
 
     writer=csv.writer(csv_file,delimiter=",", lineterminator='\n') # create a csv writer to write the job ad text and job type for each job
     
+    visited_jobs = set()
+
     for list_page in range(0,page_num): # for each page 
         
         print ('Scraping page', str(list_page+1))
@@ -43,6 +45,9 @@ def run_scraper(url,page_num, job_type):
             print("Processing job #", job_idx, "from page", str(list_page+1))
             job_url = str("https://www.indeed.com" + job_ad.get("href")) # complete the url since only the tail of it is given
 
+            if job_url in visited_jobs:
+                continue
+
             for i in range(5): # try 5 times
                 # send a request to access the job_url
                 response = requests.get(job_url ,headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', })
@@ -59,21 +64,31 @@ def run_scraper(url,page_num, job_type):
             # as per the deliverables, save the html for each job
             current_dir = os.path.dirname(os.path.realpath(__file__))
             if not os.path.exists(os.path.join(current_dir, str("jobs/" + job_type))):
-                os.mkdir(os.path.join(current_dir, str("jobs/"+job_type))
+                os.makedirs(os.path.join(current_dir, str("jobs/"+job_type)))
             
-            with io.open(str("jobs/" + job_type + "/job_" + str(job_idx) + "page_" + str(list_page+1), "w", encoding="utf-8") as html_file:
+            with io.open("jobs/" + str(job_type) + "/page_" + str(list_page+1) + "_job_" + str(job_idx+1) , "w", encoding="utf-8") as html_file:
                 html_file.write(job_html)
                 html_file.close()
 
             jobSoup = BeautifulSoup(job_html, 'html') # parse the html of the job using beautiful soup so we can retrieve the relevant text
 
             job_description = jobSoup.find('div', {'id':'jobDescriptionText'}) #find the description of the job 
-
-            writer.writerow([job_description.get_text(), job_type])
+            if job_description != None:
+                writer.writerow([job_description.get_text(), job_type])
+            
+            visited_jobs.add(job_url)
 
             
     print("finished")
     csv_file.close()
 
 
-run_scraper("https://www.indeed.com/jobs?q=Data+Engineer&l=Washington%2C+DC&radius=100", 10, "data engineer")
+run_scraper("https://www.indeed.com/jobs?q=Data+Engineer&l=Washington%2C+DC&radius=100", 990, "data engineer")
+run_scraper("https://www.indeed.com/jobs?q=data+engineer&l=Princeton%2C+NJ&radius=100", 990, "data engineer")
+run_scraper("https://www.indeed.com/jobs?q=data+engineer&l=Sacramento%2C+CA&radius=100", 980, "data engineer")
+run_scraper("https://www.indeed.com/jobs?q=software+engineer&l=Washington%2C+DC&radius=100", 940, "software engineer")
+run_scraper("https://www.indeed.com/jobs?q=software+engineer&l=Princeton%2C+NJ&radius=100", 930, "software engineer")
+run_scraper("https://www.indeed.com/jobs?q=software+engineer&l=Sacramento%2C+CA&radius=100", 990, "software engineer")
+run_scraper("https://www.indeed.com/jobs?q=data+scientist&l=Washington%2C+DC&radius=100", 690, "data scientist")
+run_scraper("https://www.indeed.com/jobs?q=data+scientist&l=Princeton%2C+NJ&radius=100", 670, "data scientist")
+run_scraper("https://www.indeed.com/jobs?q=data+scientist&l=Sacramento%2C+CA&radius=100", 680, "data scientist")
